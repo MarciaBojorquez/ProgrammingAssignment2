0e5962924add3b6fc79ba225513150bb0b07c202
Marcia Vianey Bojorquez Avitia <bojorquezmarcia@gmail.com>
2016-03-20 21:25:48 -0600

    Calculates the inverse of a matrix, if it is already been calculated it access the value stored in the cache

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Marcia Vianey Bojorquez Avitia
+## Coursera: Module 2, week 3
 
-## Write a short comment describing this function
+
+## Get the inverse of a matrix, if it has been already calculated the 
+## already computed value
+
+## Make a special matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Solve the inverse of a matrix, if it has been already calculated
+## the the inverse from the cache. Otherwise, calculate the inverse
 
+## x our special matrix
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
 }
